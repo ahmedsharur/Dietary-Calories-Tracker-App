1c0514c2f2cb67d21c9ca90c62a092763fef9696
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-10-15 09:57:01 -0500

    added a delete saga

--- src/redux/sagas/delete.saga.js
+++ src/redux/sagas/delete.saga.js
@@ -1,18 +1,18 @@
-// import { put, takeLatest } from 'redux-saga/effects'
-// import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects'
+import axios from 'axios';
 
-// function* deleteFood(action) {
-//     try {
-//         console.log("Item to delete: ", action.payload);
-//         yield axios.delete(`/api/foods/${action.payload}`);
-//         yield put({ type: 'FETCH_FOOD' });
-//     } catch (error) {
-//         console.log('could not delete', error);
-//     }
-// }
+function* deleteFood(action) {
+    try {
+        console.log("Item to delete: ", action.payload);
+        yield axios.delete(`/api/foods/${action.payload}`);
+        yield put({ type: 'FETCH_FOOD' });
+    } catch (error) {
+        console.log('could not delete', error);
+    }
+}
 
-// function* deleteSaga() {
-//     yield takeLatest('DELETE_FOOD', deleteFood)
-// }
+function* deleteSaga() {
+    yield takeLatest('DELETE_FOOD', deleteFood)
+}
 
-// export default deleteSaga;
+export default deleteSaga;
